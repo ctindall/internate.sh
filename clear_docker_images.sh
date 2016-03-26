c8064881f6db3f0689297470e59bb73ae2c1e418
Cameron Tindall <ctindall@gmail.com>
2016-03-26 13:10:23 -0500

    fix docker cleanup script to use proper binary path

--- clear_docker_images.sh
+++ clear_docker_images.sh
@@ -2,7 +2,7 @@
 
 eval $(docker-machine env --shell bash "$1")
 
-/usr/local/bin/docker rmi $(/usr/local/bin/docker images -qa) 2&> /dev/null
-/usr/local/bin/docker rm $(/usr/local/bin/docker ps -qa) 2&> /dev/null
+/usr/bin/docker rmi $(/usr/bin/docker images -qa) 2&> /dev/null
+/usr/bin/docker rm $(/usr/bin/docker ps -qa) 2&> /dev/null
 
 exit 0
